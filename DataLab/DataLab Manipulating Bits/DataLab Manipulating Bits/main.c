408a02d4b304e95017de6aae95911687b0aa8da3
Ryan Vary <ryanvary@gmail.com>
2017-09-21 15:00:21 -0600

    Initial Commit

---
+++ DataLab/DataLab Manipulating Bits/DataLab Manipulating Bits/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataLab Manipulating Bits
+//
+//  Created by Ryan Vary on 9/21/17.
+//  Copyright (c) 2017 Ryan Vary. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
